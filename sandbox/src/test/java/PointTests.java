11b30c3dae306cccc3e702124ed573d1432ffb2b
zireaelGS <zireaelGS@gmail.com>
2020-10-27 18:20:06 +0300

    Задание 3. Подключение тестового фреймворка и первые тесты

---
+++ sandbox/src/test/java/PointTests.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.testng.Assert;
+
+public class PointTests {
+    @Test
+    public void testDistance() {
+        Point point1 = new Point(10, 34);
+        Point point2 = new Point(3, 10);
+        Assert.assertEquals(point1.distance(point2), 25.0);
+    }
+
+    @Test
+    public void testTheCoincidenceOfPoints() {
+        Point point1 = new Point(10, 34);
+        Point point2 = new Point(10, 34);
+        Assert.assertEquals(point1.x, point2.x);
+        Assert.assertEquals(point1.y, point2.y);
+    }
+
+    @Test
+    public void testDistanceIsPositiveValue() {
+        Point point1 = new Point(10, 34);
+        Point point2 = new Point(3, 10);
+        Assert.assertTrue(point1.distance(point2) > 0.0);
+    }
+
+    @Test
+    public void testPoint1IsHighterThanPoint2() {
+        Point point1 = new Point(10, 34);
+        Point point2 = new Point(3, 10);
+        Assert.assertTrue(point1.x > point2.x);
+    }
+}
