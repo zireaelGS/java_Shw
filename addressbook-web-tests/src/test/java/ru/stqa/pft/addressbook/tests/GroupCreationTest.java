d9432fd7a34f7dd4714e32141ffa207313dc0408
zireaelGS <60180962+zireaelGS@users.noreply.github.com>
2020-12-01 03:37:19 +0300

    Замена вычисления максимального идендификатора для создания групп на лямбда выражение.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,13 +21,7 @@ public class GroupCreationTest extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);//check for compliance with quantity
         
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);//add a new group to the collection that was before changes
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));//convert two lists into sets and compare without regard to order
         app.getSessionHelper().logout();
