6a416991d025ee56d348c885f3ea1e477055a05b
zireael <zireaelsg@gmail.com>
2020-12-20 20:04:07 +0300

    Передача файлов на удалённую машину по протоколу FTP
    Лекция 8.5.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -45,10 +46,17 @@ public class ApplicationManager {
     }
 
     public RegistrationHelper registrtion() {
-        if(registrationHelper==null) {
+        if (registrationHelper == null) {
             registrationHelper = new RegistrationHelper(this);
         }
         return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
     }
 
     public WebDriver getDriver() {
