ef0dc013e30bc33b7ab26e9e82a64e135cde747c
zireael <zireaelsg@gmail.com>
2020-12-26 20:43:57 +0300

    Решение проблемы с RestAssured

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
